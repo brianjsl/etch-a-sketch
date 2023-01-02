d314c863b89132cf22a01330fc6ac0035175d67c
Brian Lee <brianjslmit@gmail.com>
2023-01-02 12:21:50 +0900

    Revert to Border Styling and Variable size Canvas
    Now generalizes the createDivGrid to including variable width/height
    through use of parameters and a border creating function in addBorder.

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function createDivGrid() {
+function addBorder(square, i,j,width, height, thickness) {
+    if (i === 0) {
+        square.style.borderTop = `${thickness}px solid black`;
+        if (j === 0) {
+            square.style.borderLeft = `${thickness}px solid black`;
+        }
+        else if (j === width-1) {
+            square.style.borderRight = `${thickness}px solid black`;
+        }
+    }
+
+    if (i === height - 1) {
+        square.style.borderBottom = `${thickness}px solid black`;
+        if (j === 0) {
+            square.style.borderLeft = `${thickness}px solid black`;
+        }
+        else if (j === width-1) {
+            square.style.borderRight = `${thickness}px solid black`;
+        }
+    }
+
+    if (j === 0 && (i !== 0 || i !== width - 1)) {
+        square.style.borderLeft = `${thickness}px solid black`;
+    }
+
+    if (j == width -1 && (i !== 0 || i !== width -1)) {
+        square.style.borderRight = `${thickness}px solid black`;
+    }
+}
+
+function createDivGrid(width, height) {
     let squareContainer = document.querySelector(".content > .squares");
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < height; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
+        row.style = "display: flex"
 
-        for (let j = 0; j< 16; j++) {
+        for (let j = 0; j< width; j++) {
             let square = document.createElement('div');
             square.classList.add('square');
+            square.style = "padding: 12px; background-color: white; border: 0; margin: 0;";
+            addBorder(square, i, j, width, height, 2);
+
             row.appendChild(square);
         }
         
@@ -15,4 +49,4 @@ function createDivGrid() {
     }
 }
 
-createDivGrid();
+createDivGrid(16, 16);
